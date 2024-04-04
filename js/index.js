0e864d1e3d347859a035ae223f3885ac51c9b356
Felix Kirchhoff <f.kirchhoff@proton.me>
2024-04-04 11:07:26 +0200

    initial commit

---
+++ js/index.js
@@ -0,0 +1,45 @@
+console.log("hello world!");
+console.log("JS Rocks!");
+
+const message = "This is a variable.";
+
+// String vs variable: its all about the ""
+console.log(message); // -> this is a variable
+console.log("message"); // -> message
+
+// document === DOM (Document Object Model)
+const title = document.querySelector('[data-js="title"]');
+
+console.log(title);
+
+title.textContent = "Hello World";
+title.classList;
+title.addEventListener;
+
+// Actions on our website (i.e. by users) are called events
+
+title.addEventListener("click", function () {
+  console.log("Title clicked!");
+});
+
+// Dark mode logic
+const bodyElement = document.querySelector("[data-js=body]");
+const darkModeButton = document.querySelector("[data-js=dark-mode-button]");
+const lightModeButton = document.querySelector("[data-js=light-mode-button]");
+console.log(lightModeButton, "second");
+const toggleButton = document.querySelector("[data-js=toggle-mode-button]");
+
+darkModeButton.addEventListener("click", () => {
+  bodyElement.classList.add("dark");
+  title.textContent = "Dark Mode";
+});
+
+lightModeButton.addEventListener("click", () => {
+  bodyElement.classList.remove("dark");
+  title.textContent = "Light Mode";
+});
+
+toggleButton.addEventListener("click", () => {
+  bodyElement.classList.toggle("dark");
+  title.textContent = "No idea, the other thing.";
+});
